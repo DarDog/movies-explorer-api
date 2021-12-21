0c576caf0afae680e1e7b11dc7615026bbab605d
DarDog <samediacc666@gmial.com>
2021-12-21 22:40:08 +1100

    Feat: add route for registration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errorHandler } = require('./middlewares/errors');
+const { celebrate, Joi } = require('celebrate');
+const { setUser } = require('./controllers/users')
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -9,6 +11,22 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/movies-explorer-db');
 
 app.use(bodyParser.json());
+
+app.post('/signup', celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .email()
+        .required(),
+      name: Joi.string()
+        .min(2)
+        .max(30),
+      password: Joi.string()
+        .required()
+        .min(8)
+        .max(35),
+    }),
+}), setUser)
 
 app.use((req, res, next) => {
   req.user = {
