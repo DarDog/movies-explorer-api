3897c06095cd5a732d611e76888149f6290abf6c
DarDog <samediacc666@gmial.com>
2021-12-21 18:46:43 +1100

    Feat: add routes for movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,46 @@
 const router = require('express')
   .Router();
 const { celebrate, Joi } = require('celebrate');
+const regExp = require('../regexp/regexp')
 
-router.get
+const { getMovies, setMovie, removeMovie } = require('../controllers/movies')
+
+router.get('/', getMovies);
+router.post('/', celebrate({
+  body: Joi.object({
+    country: Joi.string()
+      .required(),
+    director: Joi.string()
+      .required(),
+    duration: Joi.number()
+      .required(),
+    year: Joi.string()
+      .required(),
+    description: Joi.string()
+      .required(),
+    image: Joi.string()
+      .pattern(regExp)
+      .required(),
+    trailer: Joi.string()
+      .pattern(regExp)
+      .required(),
+    thumbnail: Joi.string()
+      .pattern(regExp)
+      .required(),
+    movieId: Joi.string()
+      .required(),
+    nameRU: Joi.string()
+      .required(),
+    nameEN: Joi.string()
+      .required(),
+  })
+}) ,setMovie);
+router.delete('/:movieId', celebrate({
+  params: Joi.object()
+    .keys({
+      movieId: Joi.string()
+        .hex(),
+    })
+}),removeMovie);
+
+module.exports = router;
