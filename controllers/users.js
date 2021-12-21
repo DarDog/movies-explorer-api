a916448decaa5add1b3399c43887ab63a8d92799
DarDog <samediacc666@gmial.com>
2021-12-21 22:55:57 +1100

    Feat: add controller for registration

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 const NotFoundError = require('../errors/NotFound');
 const BadRequestError = require('../errors/BadRequest');
+const ConflictError = require('../errors/Conflict');
 
 module.exports.getUserInfo = (req, res, next) => {
   User.findById(req.user._id)
@@ -25,7 +27,7 @@ module.exports.updateUserInfo = (req, res, next) => {
     .orFail(new Error('InvalidId'))
     .then(user => res.send(user))
     .catch(err => {
-      console.log(err)
+      console.log(err);
       if (err.message === 'InvalidId') {
         next(new NotFoundError(`Пользователь с ID: ${req.user._id} не найден`));
       } else if (err.name === 'ValidationError') {
@@ -34,5 +36,33 @@ module.exports.updateUserInfo = (req, res, next) => {
         next(new BadRequestError('Введен некорректный ID'));
       }
       next(err);
+    });
+};
+
+module.exports.setUser = (req, res, next) => {
+  const { email, name, password } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then(hash => User.create({
+      email,
+      name,
+      password: hash,
+    }))
+    .then(() =>
+      res.send({
+        data: {
+          name,
+          email
+        }
+      })
+    )
+    .catch(err => {
+      if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже зарегистрирован'));
+      } else if (err.name === 'ValidationError') {
+        next(new BadRequestError(err.message));
+      } else {
+        next(err);
+      }
     });
 };
