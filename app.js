7083b8446bac89c1b4f531dbc72226034853b88c
DarDog <samediacc666@gmial.com>
2021-12-21 14:30:17 +1100

    Feat: add Error middleware

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errorHandler } = require('./middlewares/errors')
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/movies-explorer-db');
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
